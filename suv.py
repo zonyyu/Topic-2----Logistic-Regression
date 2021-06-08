bb4e51c8ef2370c41d8a23bdc53253bde18e8ab7
Zony Yu <zony249@gmail.com>
2021-06-08 14:19:00 -0600

    blanked everything

--- suv.py
+++ suv.py
@@ -1,22 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 import requests
 
-# import all of these modules
-import numpy as np
-import import_ipynb # used to import classes from notebooks
-from suv_purchase import Logreg
-import torch
-from sklearn.preprocessing import PolynomialFeatures
 
-
-
-model = Logreg(20, 1) # initialize a new model
-model.load_state_dict(torch.load('suv_predictor.pt')) # load trained weights and bias from file
-model.eval() # set it to eval mode
-
-# We need to convert 3 input features into the 20 polynomial features
-# that the model can accept
-poly = PolynomialFeatures(degree=3) 
 
 
 app = Flask(__name__)
@@ -34,46 +19,10 @@ def get_weather():
     age = request.args.get("age")
     salary = request.args.get("salary")
 
-    # Convert gender to lower case, and handle invalid inputs
-    gender = gender.lower()
-    if gender == 'male':
-        gender = 0
-    elif gender == 'female':
-        gender = 1
-    elif gender == 'other':
-        gender = 1
-    else:
-        return jsonify({"code": 500, "msg": "Invalid Gender"})
 
-
-    # Age must be integer
-    try:
-        age = int(age)
-    except ValueError:
-        return jsonify({"code": 500, "msg": "Invalid Age"})
-
-    
-    # Salary must be integer
-    try:
-        salary = int(salary)
-    except ValueError:
-        return jsonify({"code": 500, "msg": "Invalid salary"})
-
-
-    # form input variables into a 1x20 torch.tensor
-    inputs_raw = np.array([[age, salary, gender]])
-    inputs_poly = poly.fit_transform(inputs_raw)
-    inputs = torch.from_numpy(inputs_poly).float()
-
-    # make prediction
-    pred = model(inputs).item() #.item() gets the value from the tensor
-    result = "YES"
-    if pred < 0.5:
-        result = "NO"
-    
 
    
-    return jsonify({"code": 200, "msg": result})
+    return jsonify({"code": 200, "msg": None})
 
 
 if __name__ == "__main__":
